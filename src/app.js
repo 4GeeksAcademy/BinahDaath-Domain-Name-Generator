095639ca91fcac4804cad21ffa08bad8de540231
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-06-04 00:55:41 +0000

    in progress

--- src/app.js
+++ src/app.js
@@ -11,6 +11,15 @@ window.onload = function() {
   const adjs = ["great", "big"];
   const nouns = ["jogger", "racoon"];
   const domains = [];
-
+  for (const p in pronouns) {
+    for (const a in adjs) {
+      for (const n in nouns) {
+        domains.push(pronouns[p] + adjs[a] + nouns[n] + ".com");
+      }
+    }
+  }
+  const innerHtml = domains.join("\n");
+  const p = document.querySelector("#Domains");
+  p.innerHTML = innerHtml;
   console.log("Hello Rigo from the console!");
 };
