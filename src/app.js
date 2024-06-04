8e3242fe2282416be482f5a207a1976aab94fb77
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-06-04 00:34:23 +0000

    in progress

--- src/app.js
+++ src/app.js
@@ -7,5 +7,9 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const pronouns = ["the", "our"];
+  const adjs = ["great", "big"];
+  const nouns = ["jogger", "racoon"];
+  const domains = [];
   console.log("Hello Rigo from the console!");
 };
